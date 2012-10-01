c79f716018861b26c500c15f8fd7cb3741aa725a
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-10-01 12:33:49 +0400

    Initial

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='thebot-instagram',
+    version='0.1.0',
+    description='Prints a new popular photo from the instagram each hour.',
+    keywords='thebot instagram plugin',
+    license = 'New BSD License',
+    author="Alexander Artemenko",
+    author_email='svetlyak.40wt@gmail.com',
+    url='http://github.com/svetlyak40wt/thebot-instagram/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Server',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'thebot',
+        'python-instagram',
+    ],
+)
---
+++ thebot_instagram.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import
+
+import thebot
+import threading
+import time
+import random
+
+from instagram.client import InstagramAPI
+
+
+class Plugin(thebot.Plugin):
+    # each 15 minutes
+    INTERVAL = 60 * 15
+
+    def get_callbacks(self):
+        return [
+            ('instagram on', self.on),
+            ('instagram off', self.off),
+        ]
+
+    def worker(self):
+        api = InstagramAPI(client_id='1a748d8ab1ad48ab8e97b63c5d962355', client_secret='c9388c7acb064b779a7e5a3b4c5a7c12')
+        countdown = 0
+
+        while not self._event.is_set():
+            if countdown == 0:
+                photos = api.media_popular(count=20)
+                photo = random.choice(photos)
+                self._request.respond(photo.images['standard_resolution'].url)
+                countdown = self.INTERVAL
+            else:
+                countdown -= 1
+
+            time.sleep(1)
+
+        self._request.respond('instagram was turned off')
+
+    def on(self, request, match):
+        thread = getattr(self, '_thread', None)
+        if thread is not None and thread.is_alive():
+            return
+
+        self._event = threading.Event()
+        self._request = request
+        self._thread = threading.Thread(target=self.worker)
+        self._thread.daemon = True
+        self._thread.start()
+
+        request.respond('instagram was turned on')
+
+    def off(self, request, match):
+        event = getattr(self, '_event', None)
+        if event is not None:
+            event.set()
+
